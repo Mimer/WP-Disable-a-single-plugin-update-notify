b8e2f73b3a49e0c2732b7224da219b55670b7a33
Mimer <mimer@epicdesigngt.com>
2012-09-18 10:14:41 -0600

    add the filter and! have fun!
    just add the code in the main plugin php file and that's all!

---
+++ themainpluginfile.php
@@ -0,0 +1,6 @@
+add_filter('site_transient_update_plugins', 'dd_remove_update_nag');
+function dd_remove_update_nag($value) {
+ unset($value->response[ plugin_basename(__FILE__) ]);
+ return $value;
+}
+
